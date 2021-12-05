e8faf024c7df5de641bc09c99a188ac40b77449b
Carly Bergthold <carlybergthold@gmail.com>
2021-12-05 12:35:19 -0600

    day 2

--- day-2/day-2.js
+++ day-2/day-2.js
@@ -1,29 +1,39 @@
-// --- Day 2: Dive! ---
-// Now, you need to figure out how to pilot this thing.
+const sonarSweeps = require('./input');
 
-// It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
+// Part 1
+let horizontalPosition = 0;
+let depth = 0;
 
-// forward X increases the horizontal position by X units.
-// down X increases the depth by X units.
-// up X decreases the depth by X units.
-// Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
+sonarSweeps.forEach(sweep => {
+    const splitArray = sweep.split(' ');
+    const command = splitArray[0];
+    const units = parseInt(splitArray[1]);
 
-// The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
+    if (command === 'forward') {
+        horizontalPosition += units;
+    } else {
+        command === 'down' ? depth += units : depth -= units;
+    }
+})
 
-// forward 5
-// down 5
-// forward 8
-// up 3
-// down 8
-// forward 2
-// Your horizontal position and depth both start at 0. The steps above would then modify them as follows:
+console.log('Our final horizontal position of ' + horizontalPosition + ' multiplied by our final depth of ' + depth + ' = ' + (horizontalPosition * depth));
 
-// forward 5 adds 5 to your horizontal position, a total of 5.
-// down 5 adds 5 to your depth, resulting in a value of 5.
-// forward 8 adds 8 to your horizontal position, a total of 13.
-// up 3 decreases your depth by 3, resulting in a value of 2.
-// down 8 adds 8 to your depth, resulting in a value of 10.
-// forward 2 adds 2 to your horizontal position, a total of 15.
-// After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
+// Part 2
+horizontalPosition = 0;
+depth = 0;
+let aim = 0;
 
-// Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+sonarSweeps.forEach(sweep => {
+    const splitArray = sweep.split(' ');
+    const command = splitArray[0];
+    const units = parseInt(splitArray[1]);
+
+    if (command === 'forward') {
+        horizontalPosition += units;
+        depth += (aim * units);
+    } else {
+        command === 'down' ? aim += units : aim -= units;
+    }
+})
+
+console.log('Our final horizontal position of ' + horizontalPosition + ' multiplied by our final depth of ' + depth + ' = ' + (horizontalPosition * depth));
